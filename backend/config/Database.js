9edd2f903a963e302a18dfc40e50e2bc9f4b538a
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-03-03 10:09:12 +0700

    update connection 2

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -6,7 +6,9 @@ const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'postgres',
-    dialectOptions: {}
+    dialectOptions: {
+        ssl: true
+    }
 })
 
 module.exports = db;
