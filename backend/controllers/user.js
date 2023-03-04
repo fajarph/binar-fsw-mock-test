33d7e020fd2c16d136fda94e72cf5b1b472569b0
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-03-04 11:34:29 +0700

    update in user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,10 +3,21 @@ const User = require('../models/user.js')
 const createUser = async(req, res) => {
     try {
         const {name, pin} = req.body;
+        const user = await User.findOne({
+            where: {
+                pin: pin
+            }
+        });
+
+        console.log(user);
+
+        if (user) return res.status(400).json({msg: "Pin sudah terdaftar"})
+
         await User.create({
             name: name,
             pin: pin,
         })
+
         res.status(201).json({msg: "User Created"})
     } catch (error) {
         res.status(400).json({msg: error.message})
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
             })
             navigate("/login")
         } catch (error) {
-            console.log(error);
+            alert("Pin sudah ada")
         }
     }
 
