887da11e464e0d726dbd25c967bc08ed5c7ed4a4
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-03-04 12:35:14 +0700

    update session config

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,13 @@ const store = new sessionStore({
 
 app.use(session({
     secret: process.env.SESS_SECRET,
+    saveUninitialized: false,
     resave: false,
     store: store,
+    proxy: true,
     cookie: {
         secure: 'auto'
-    },
-    proxy: true
+    }
 }))
 
 app.use(cors({
