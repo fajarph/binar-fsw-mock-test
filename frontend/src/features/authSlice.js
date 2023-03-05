049bf6310d6f9ad60c37b419b9aa5ddabf0f809f
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-03-05 18:54:56 +0700

    Update get request

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -25,7 +25,7 @@ export const LoginUser = createAsyncThunk("user/LoginUser", async(user, thunkApi
 
 export const getMe = createAsyncThunk("user/getMe", async(_, thunkApi) => {
     try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/me`, {}, { withCredentials: true }) 
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/me`, { withCredentials: true }) 
         return response.data
     } catch (error) {
         if(error.response){
